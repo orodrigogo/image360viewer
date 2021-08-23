c8639a6343f55a728b5b09d4f62ae295a587d994
rodrigo <rodrigo@pop-os.localdomain>
2021-08-23 07:31:57 -0300

    feat: image 360 viewer

---
+++ index.tsx
@@ -0,0 +1,88 @@
+import React, {
+  useState,
+  useRef,
+} from 'react';
+import {
+  View,
+  Image,
+  ImageProps,
+  ImageResizeMode,
+  PanResponder,
+  PanResponderGestureState,
+} from 'react-native';
+
+type Props = {
+  height?: number;
+  width?: number;
+  images: ImageProps[];
+  resizeMode?: ImageResizeMode;
+  rotationRatio?: number; // 
+}
+
+/**
+ * This is a library that shows images at 350 degrees.
+ * @param {ImageProps[]} images 
+ * - Vector images to rotate.
+ * @param {number} height 
+ * - Image height default is 250.
+ * @param {number} width 
+ * - Image width default is 250.
+ * @param {number} rotationRatio 
+ * - The drag distance compares to 180 degree. 
+ * Example: width / rotationRatio = 180 degree. The value default is 0.5.
+ * @param {string} resizeMode 
+ * - Image display mode. Default is contain.
+ */
+
+function Image360viewer({
+  images,
+  height = 250,
+  width = 250,
+  rotationRatio = 0.5,
+  resizeMode = 'contain'
+}: Props) {
+  const [imageIndexSelected, setImageIndexSelected] = useState(0);
+
+  const rotation = useRef({ value: width / 2 });
+  const startRotation = useRef({ value: 0 });
+  const rotatePeriod = useRef({ value: 360 / images.length });
+
+  const startPositionXAxis = useRef({ value: 0 });
+  const currentPositionXAxis = useRef({ value: 0 });
+
+  const panResponder = useRef(PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderGrant: (_, gestureState) => {
+      startPositionXAxis.current.value = gestureState.moveX;
+    },
+    onPanResponderMove: (_, gestureState) => {
+      handleUpdateRotation(gestureState);
+    },
+    onPanResponderRelease: (_, gestureState) => {
+      handleUpdateRotation(gestureState);
+    }
+  })).current;
+
+  function handleUpdateRotation(gestureState: PanResponderGestureState) {
+    startPositionXAxis.current.value = gestureState.moveX;
+
+    const deltaRotation = (currentPositionXAxis.current.value - startPositionXAxis.current.value) * 180 / (rotationRatio * width);
+    rotation.current.value = startRotation.current.value + deltaRotation;
+
+    const mRotation = rotation.current.value - Math.floor(rotation.current.value / 360) * 360;
+    setImageIndexSelected(Math.floor(mRotation / rotatePeriod.current.value));
+  }
+
+  return (
+    <View {...panResponder.panHandlers} >
+      <Image
+        source={images[imageIndexSelected]}
+        style={{ width, height }
+        }
+        resizeMode={resizeMode}
+      />
+    </View>
+  );
+}
+
+export { Image360viewer }
